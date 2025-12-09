9a32cd1213e0b7cea1ad9ba8d2cb1cf10c8a615d
alstjr4814 <alstjr48144814@gmail.com>
2025-12-09 17:15:18 +0900

    dd

--- src/main/java/com/korit/springboot/mapper/UserMapper.java
+++ src/main/java/com/korit/springboot/mapper/UserMapper.java
@@ -8,5 +8,6 @@ import org.apache.ibatis.annotations.Param;
 public interface UserMapper {
     int insert(UserEntity userEntity); //user.xml 안의 <insert id="insert"> 실행시키는 sql
     UserEntity findUserByUsername(@Param("username") String username);
+    UserEntity findUserByUserId(@Param("userId") int userId);
 
 }
